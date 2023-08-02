a48be5878d5b861ce511c27a4178fca4705f17ac
slmtwn <slmtwn@gmail.com>
2023-08-02 22:16:43 +0700

    modifikasi chart

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -118,12 +118,24 @@
 <script>
     const ctx = document.getElementById('myChart');
     new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
             datasets: [{
-                label: 'Vote Warna',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'Rekapitulasi Pendapatan Per Bulan',
+                data: [15000, 19000, 30000, 50000, 20000, 30000],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
                 borderWidth: 1
             }]
         },
