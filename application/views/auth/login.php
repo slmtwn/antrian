7831ad3ec74b0bb2d277f9413d58f1927dcc9007
slmtwn <slmtwn@gmail.com>
2023-08-02 22:02:28 +0700

    perubahan halaman login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,13 +1,16 @@
-<div class="container mt-10">
+<div class="container">
     <!-- Outer Row -->
+    <div class="container">
+        <div class="row mt-5">
+        </div>
+    </div>
     <div class="row justify-content-center mt-lg-auto">
-        <div class="col-xl-10 col-lg-6 col-md mt-10">
+        <div class="col-lg-6 mt-5 my-lg-5">
             <div class="card o-hidden border-0 shadow-lg my-auto">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
-                    <div class="row mt-auto">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                        <div class="col-lg-6">
+                    <div class="row mt-4">
+                        <div class="col-lg">
                             <div class="p-5">
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Halaman Login</h1>
